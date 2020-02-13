1e63d9252a282ead564f16c615d0c7cdd96d8c68
Gaurav Tyagi <gtyagi1@gmail.com>
2020-02-13 10:31:20 -0500

    my second commit

---
+++ src/io/mentortech/util/Main.java
@@ -0,0 +1,10 @@
+package io.mentortech.util;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+        SupportClass s = new SupportClass();
+        System.out.println(s.add(2,34));
+    }
+}
